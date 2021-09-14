f4ca891fdb3e5adf8b37895050e2e589ea98fec4
Gavriil84 <79692322+Gavriil84@users.noreply.github.com>
2021-09-13 23:10:28 -0700

    Created template for user card

---
+++ src/components/organism/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img src="" alt="profile" />
+      <p>Name</p>
+      <dl>
+        <dt>Mail</dt>
+        <dd>111111@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>123-456-7890</dd>
+        <dt>Company</dt>
+        <dd>AAAAAAA</dd>
+        <dt>WEB</dt>
+        <dd>aaaaaaaaaa.com</dd>
+      </dl>
+    </div>
+  );
+};
